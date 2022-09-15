2a887b8899f7bf93edca6d48c092b4021376b032
Tanver-Hasan <38843176+Tanver-Hasan@users.noreply.github.com>
2022-09-15 16:14:03 +0100

    initail setup

---
+++ build/precache-manifest.caeae0995386d99fd8b1c4acdc4e715a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b6009ea60338f125144146eecd8aab1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d575b0c96f7c51ed02d7",
+    "url": "/static/css/2.11b452ed.chunk.css"
+  },
+  {
+    "revision": "3ade17a0b35882df22fd",
+    "url": "/static/css/main.1aa3f63c.chunk.css"
+  },
+  {
+    "revision": "d575b0c96f7c51ed02d7",
+    "url": "/static/js/2.3f16f996.chunk.js"
+  },
+  {
+    "revision": "0601554d6437e8ab1ea2021021d2eb3b",
+    "url": "/static/js/2.3f16f996.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ade17a0b35882df22fd",
+    "url": "/static/js/main.6c304d43.chunk.js"
+  },
+  {
+    "revision": "0231f95ac667b73c9187",
+    "url": "/static/js/runtime-main.91e21c86.js"
+  },
+  {
+    "revision": "c5590569aab553af03722baa7d379978",
+    "url": "/static/media/loading.c5590569.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
